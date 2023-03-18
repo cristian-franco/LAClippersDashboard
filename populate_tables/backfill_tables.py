9123b31421ffaa3726cf45c372b17c5c4125e86c
cristian-franco <cristianfranco.cf@gmail.com>
2023-03-17 18:57:57 -0700

    Organized functions, format_db function mostly complete

--- populate_tables/backfill_tables.py
+++ populate_tables/backfill_tables.py
@@ -4,52 +4,52 @@ import pandas as pd
 from requests.exceptions import Timeout
 
 
+# EXTRACT
 def get_clips_id():
-    clips = teams.find_team_by_abbreviation('lac')
-    clips_id = clips["id"]
+    clips_info = teams.find_team_by_abbreviation('lac')
+    clips_id = clips_info["id"]
 
     return clips_id
 
 
-def gamefinder(team_id, int_timeout):
+def game_finder(team_id, int_timeout):
+    # default param for LeagueGameFinder is current season
     try:
         games_found = leaguegamefinder.LeagueGameFinder(team_id_nullable=team_id, timeout=int_timeout)
     except Timeout:
         print('Timeout caught, retying...')
-        games_found = gamefinder(team_id, int_timeout)
+        games_found = game_finder(team_id, int_timeout)
 
-    print('Games found! Step 1 - Done')
     return games_found
 
 
-def get_boxscores(game_id, int_timeout):
-    try:
-        game_sets = boxscoretraditionalv2.BoxScoreTraditionalV2(game_id, timeout=int_timeout)
-    except Timeout:
-        print('Timeout caught, retrying...')
-        game_sets = get_boxscores(game_id, int_timeout)
-
-    print('Boxscores pulled!')
-    return game_sets
-
-
 def get_current_season_game_ids(clips_id):
-    # default param for LeagueGameFinder is current season
-    games_found = gamefinder(clips_id, 10)
-
-    games = games_found.get_data_frames()[0]
+    # contains summer league and preseason games
+    games = game_finder(clips_id, 10).get_data_frames()[0]
     current_season_id = games['SEASON_ID'].iloc[0]
+
     current_season_games = games[games.SEASON_ID == current_season_id]
     current_season_games = current_season_games.loc[current_season_games['GAME_DATE'] > '2022-10-01']
 
     current_season_game_ids = list(current_season_games['GAME_ID'])
-    # this contains summer league and preseason games
+
     return current_season_game_ids
 
 
-def get_game_player_stats(clips_id, game_id):
-    game_sets = get_boxscores(game_id, 10)
+def get_box_score(game_id, int_timeout):
+    try:
+        game_sets = boxscoretraditionalv2.BoxScoreTraditionalV2(game_id, timeout=int_timeout)
+    except Timeout:
+        print('Timeout caught, retrying...')
+        game_sets = get_box_score(game_id, int_timeout)
 
+    print('Box score pulled!')
+
+    return game_sets
+
+
+def get_game_df(clips_id, game_id):
+    game_sets = get_box_score(game_id, 10)
     game_df = game_sets.get_data_frames()[0]
     game_df = game_df.loc[game_df['TEAM_ID'] == clips_id]
     game_df = game_df.drop(['TEAM_ABBREVIATION', 'TEAM_CITY', 'NICKNAME', 'COMMENT'], axis=1)
@@ -57,13 +57,12 @@ def get_game_player_stats(clips_id, game_id):
     return game_df
 
 
-def season_to_date_team(clips_id, current_season_game_ids):
-    # get the dataframes of all the games then combine them into one
+def get_season_to_date_box_scores(clips_id, current_season_game_ids):
     game_df = pd.DataFrame()
     tracker = 0
     for game_id in current_season_game_ids:
         try:
-            game_df = pd.concat([game_df, get_game_player_stats(clips_id, game_id)], axis=0)
+            game_df = pd.concat([game_df, get_game_df(clips_id, game_id)], axis=0)
         except Timeout:
             print('Timeout caught')
 
@@ -73,22 +72,46 @@ def season_to_date_team(clips_id, current_season_game_ids):
     return game_df
 
 
+# TRANSFORM
 def convert_minutes(minutes):
     if minutes is None:
-        print('None caught')
-        return 0
+        return '0:00'
 
-    string_minutes = str(minutes)
-    list_hh_mm = string_minutes.split(':')
-    int_minutes = str(int(float(list_hh_mm[0])))
-    formatted_minutes = int_minutes + ':' + list_hh_mm[1]
+    list_mm_ss = str(minutes).split(':')
+    int_minutes = int(float(list_mm_ss[0]))
+    int_seconds = int(list_mm_ss[1])
 
-    return formatted_minutes
+    if int_seconds >= 60:
+        int_minutes += 1
+        int_seconds -= 60
+
+    str_minutes = str(int_minutes)
+    str_seconds = str(int_seconds).zfill(2)
+    
+    return str_minutes + ':' + str_seconds
+
+
+def format_float(pct):
+    # FG_PCT, FG3_PCT, FT_PCT
+    pct = round(100 * pct, 1)
+    str_pct = str(pct) + '%'
+
+    return str_pct
+
+
+def format_floats_to_ints(flt):
+    return int(flt)
 
 
 def format_for_db(season_to_date_team_df):
-
     season_to_date_team_df['MIN'] = season_to_date_team_df['MIN'].apply(convert_minutes)
+
+    floats_columns = ['FG_PCT', 'FG3_PCT', 'FT_PCT']
+    season_to_date_team_df[str(floats_columns)] = season_to_date_team_df[str(floats_columns)].apply(format_float)
+
+    f_to_i_columns = ['FGM', 'FGA', 'FG3M', 'FG3A', 'FTM', 'FTA', 'OREB', 'DREB', 'REB', 'AST', 'STL', 'BLK', 'TO',
+    'PF', 'PTS', 'PLUS_MINUS']
+    season_to_date_team_df[str(f_to_i_columns)] = season_to_date_team_df[str(f_to_i_columns)].apply(format_floats_to_ints)
 
     return season_to_date_team_df
 
@@ -96,7 +119,9 @@ def format_for_db(season_to_date_team_df):
 def main():
     clips_id = get_clips_id()
     current_season_game_ids = get_current_season_game_ids(clips_id)
-    season_to_date_team_df = season_to_date_team(clips_id, current_season_game_ids)
+
+    season_to_date_team_df = get_season_to_date_box_scores(clips_id, current_season_game_ids)
+    
     season_to_date_team_df = format_for_db(season_to_date_team_df)
 
     return
